3a0ab784ff02543b11abf80c12fae35938830651
amparkhi21 <pkparkhikumari21@gmail.com>
2026-07-14 18:29:23 +0530

    weather

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ city.addEventListener("keydown",function(e){
         btn.click();
     }
 })
-let api="0d07b727d23a4e9b9aabc8dfd53437ab";
+let api="de9fb10660223f5bb1e7044dd1f336db";
 async function weather(c){
     try{
         const url=`https://api.openweathermap.org/data/2.5/weather?q=${c}&appid=${api}&units=metric`;
         const response=await fetch(url);
-        const data=await response.json();
+        const d=await response.json();
         if(!response.ok){
             name.textContent="City Not Found";
             document.getElementById("wind").textContent="Wind:";
@@ -30,15 +30,15 @@ async function weather(c){
             return;
         }
         if(name){
-            name.textContent=data.name;
+            name.textContent=d.name;
         }
+        document.getElementById("temp").textContent= `${d.main.temp} °C`;
+        document.getElementById("con").textContent=`${d.weather[0].main}`;
         document.getElementById("chead").textContent=`${city.value}`;
-        document.getElementById("temp").textContent= `${data.main.temp} °C`;
-        document.getElementById("con").textContent=`${data.weather[0].main}`;
-        document.getElementById("temperature").textContent = `${data.main.temp} °C`;
-        document.getElementById("humidity").textContent = `${data.main.humidity} %`;
-        document.getElementById("wind").textContent = `${data.wind.speed} km/h`;
-        document.getElementById("condition").textContent = `${data.weather[0].main}`;
+        document.getElementById("temperature").textContent = `${d.main.temp} °C`;
+        document.getElementById("humidity").textContent = `${d.main.humidity} %`;
+        document.getElementById("wind").textContent = `${d.wind.speed} km/h`;
+        document.getElementById("condition").textContent = `${d.weather[0].main}`;
     }
     catch(error){
         console.error("Error found:",error);
